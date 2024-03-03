35f19c9674aa31175eaee9e25e4f928cbdd721bc
Ikigai08 <114841248+Ikigai08@users.noreply.github.com>
2024-03-03 21:45:14 +0800

    Delete 8 TSA 5 & 6 directory

--- 8 TSA 5 & 6/script.js
+++
@@ -1,9 +0,0 @@
-const menuItems = document.querySelectorAll('.menu-item');
-
-menuItems.forEach(menuItem => {
-  menuItem.addEventListener('click', () => {
-    // Redirect to the explanation page for the dish
-    const dishId = menuItem.id.replace('menu-', '');
-    window.location.href = `dish-${dishId}.html`;
-  });
-});
